8a9fd09a461e654211ac9829a0728e4e5e276e3b
Rxshark32 <76074054+Rxshark32@users.noreply.github.com>
2024-11-07 01:26:45 -0800

    Update ssh.sh
    pasted over from masterScript

--- helperScripts/ssh.sh
+++ helperScripts/ssh.sh
@@ -1,1 +1,25 @@
+echo "Configuring OpenSSH..."
+apt-get install -y libpam-google-authenticator
+#systemctl start ssh
+sudo sed -i 's/^#Protocol.*/Protocol 2/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#MaxAuthTries.*/MaxAuthTries 3/' /etc/ssh/sshd_config
+sudo sed -i 's/^#LoginGraceTime.*/LoginGraceTime 20/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#KerberosAuthentication.*/KerberosAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#GSSAPIAuthentication.*/GSSAPIAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PermitUserEnvironment.*/PermitUserEnvironment no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#AllowAgentForwarding.*/AllowAgentForwarding no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#AllowTcpForwarding.*/AllowTcpForwarding no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PermitTunnel.*/PermitTunnel no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#DebianBanner.*/DebianBanner no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#LogLevel.*/LogLevel VERBOSE/' /etc/ssh/sshd_config
+sudo sed -i 's/^#IgnoreRhosts.*/IgnoreRhosts yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#HostbasedAuthentication.*/HostbasedAuthentication no/' /etc/ssh/sshd_config
 
+echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd
+sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
