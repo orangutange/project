ffaf06c343238319d186735d3b7fcaea4539b7c8
orangutange <gregdoucettee123@gmail.com>
2024-11-22 14:59:42 -0800

    Update samba.sh

--- helperScripts/samba.sh
+++ helperScripts/samba.sh
@@ -11,10 +11,7 @@ ufw allow microsoft-ds
 
 # Add the required configurations to the Samba global section
 echo "disable netbios = Yes" >> $smb_conf
-echo "server min protocol = SMB3" >> $smb_conf
 echo "smb ports = 445" >> $smb_conf
-echo "server signing = required" >> $smb_conf
-echo "min protocol = SMB3" >> $smb_conf
 echo "map to guest = never" >> $smb_conf
 echo "restrict anonymous = 2" >> $smb_conf
 echo "hosts allow = 127.0.0.1 192.168.1.0/24" >> $smb_conf
@@ -31,11 +28,16 @@ echo "disable spoolss = yes" >> $smb_conf
 echo "log level = 1 vfs:10" >> $smb_conf
 echo "log file = /var/log/samba/sambavfs.log" >> $smb_conf
 echo "max log size = 50" >> $smb_conf
-echo "smb encrypt = required" >> $smb_conf
-echo "client min protocol = SMB3" >> $smb_conf
-echo "client max protocol = SMB3" >> $smb_conf
+#############################################
+echo "min protocol = SMB2" >> $smb_conf
+echo "server min protocol = SMB2" >> $smb_conf
+echo "client min protocol = SMB2" >> $smb_conf
+echo "client max protocol = SMB2" >> $smb_conf
+# can change to SMB3 if doesn't give points
 echo "client signing = mandatory" >> $smb_conf
 echo "server signing = mandatory" >> $smb_conf
+echo "smb encrypt = mandatory" >> $smb_conf
+
 
 # Disable the banner
 echo "server string = " >> $smb_conf
