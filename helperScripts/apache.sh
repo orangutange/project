37b5207f8dde05aef02bdf9ac0f32db2b5c85999
orangutange <gregdoucettee123@gmail.com>
2024-12-13 23:01:09 -0800

    Update apache.sh

--- helperScripts/apache.sh
+++ helperScripts/apache.sh
@@ -5,27 +5,17 @@ systemctl start apache
 systemctl start apache2
 systemctl start httpd
 
-# Path to the new apache2.conf file
-NEW_CONF="apache2.conf"
+cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
 
-# Path to the existing apache2.conf file
-CURRENT_CONF="/etc/apache2/apache2.conf"
-
-# Create a backup of the current apache2.conf
-sudo cp "$CURRENT_CONF" "${CURRENT_CONF}.bak"
-
-# Replace the current apache2.conf with the new one
-sudo cp "$NEW_CONF" "$CURRENT_CONF"
+wget -qO- https://raw.githubusercontent.com/danvau7/very-secure-apache/refs/heads/master/apache2.conf > /etc/apache2/apache2.conf
 
 # Set correct permissions and ownership
 echo "Setting correct permissions and ownership..."
-sudo chmod 644 "$CURRENT_CONF"
-sudo chown root:root "$CURRENT_CONF"
+sudo chmod 644 "/etc/apache2/apache2.conf"
+sudo chown root:root "/etc/apache2/apache2.conf"
 
 #chown for permissions to all files
 chown -R root:root /etc/apache2
 
 # Reload Apache to apply changes
 systemctl reload apache2
-echo "Backed up '$CURRENT_CONF' to '${CURRENT_CONF}.bak', replaced it with '$NEW_CONF', and restarted Apache."
-
