50ff5aad2b36e694a2e20a4223a7ceb4e786d5b4
orangutange <gregdoucettee123@gmail.com>
2024-12-13 22:35:03 -0800

    Update samba.sh

--- helperScripts/samba.sh
+++ helperScripts/samba.sh
@@ -1,64 +1,74 @@
-echo "Configuring Samba..."
+#!/bin/bash
+
+# Install Samba
 apt-get install -y samba
 apt-get -y install system-config-samba
-    
+
 systemctl start smbd
+
 smb_conf="/etc/samba/smb.conf"
+
 ufw allow netbios-ns
 ufw allow netbios-dgm
 ufw allow netbios-ssn
 ufw allow microsoft-ds
 ufw allow 445
 
-# DOESNT ADD UNDER GLOBAL SECTION
+# Insert configurations under [global] section
+sed -i '/\[global\]/a \
+\
+disable netbios = Yes\n\
+smb ports = 445\n\
+map to guest = never\n\
+guest ok = no\n\
+restrict anonymous = 2\n\
+hosts allow = 127.0.0.1 192.168.1.0/24\n\
+hosts deny = 0.0.0.0/0\n\
+workgroup = WORKGROUP\n\
+guest account = nobody\n\
+allow insecure wide links = no\n\
+security = user\n\
+passdb backend = tdbsam\n\
+printing = bsd\n\
+printcap name = /dev/null\n\
+load printers = no\n\
+disable spoolss = yes\n\
+log level = 1 vfs:10\n\
+log file = /var/log/samba/sambavfs.log\n\
+max log size = 50\n\
+min protocol = SMB3\n\
+server min protocol = SMB3\n\
+client min protocol = SMB3\n\
+client max protocol = SMB3\n\
+ntlm auth = yes\n\
+lanman auth = no\n\
+client signing = mandatory\n\
+server signing = mandatory\n\
+smb encrypt = mandatory\n\
+server string = '" $smb_conf
 
-# Add the required configurations to the Samba global section
-echo "disable netbios = Yes" >> $smb_conf
-echo "smb ports = 445" >> $smb_conf
-echo "map to guest = never" >> $smb_conf
-echo "guest ok = no" >> $smb_conf
-echo "restrict anonymous = 2" >> $smb_conf
-echo "hosts allow = 127.0.0.1 192.168.1.0/24" >> $smb_conf
-echo "hosts deny = 0.0.0.0/0" >> $smb_conf
-echo "workgroup = WORKGROUP" >> $smb_conf
-echo "guest account = nobody" >> $smb_conf
-echo "allow insecure wide links = no" >> $smb_conf
-echo "security = user" >> $smb_conf
-echo "passdb backend = tdbsam" >> $smb_conf
-echo "printing = bsd" >> $smb_conf
-echo "printcap name = /dev/null" >> $smb_conf
-echo "load printers = no" >> $smb_conf
-echo "disable spoolss = yes" >> $smb_conf
-echo "log level = 1 vfs:10" >> $smb_conf
-echo "log file = /var/log/samba/sambavfs.log" >> $smb_conf
-echo "max log size = 50" >> $smb_conf
-#############################################
-echo "min protocol = SMB3" >> $smb_conf
-echo "server min protocol = SMB3" >> $smb_conf
-echo "client min protocol = SMB3" >> $smb_conf
-echo "client max protocol = SMB3" >> $smb_conf
-echo "ntlm auth = yes" >> $smb_conf
-echo "lanman auth = no" >> $smb_conf
-# can change to SMB3 if doesn't give points
-echo "client signing = mandatory" >> $smb_conf
-echo "server signing = mandatory" >> $smb_conf
-echo "smb encrypt = mandatory" >> $smb_conf
+# Read authorized users from file and set up their Samba accounts
+users_file="authorizedusers.txt"
+if [ -f "$users_file" ]; then
+    while IFS= read -r user || [ -n "$user" ]; do
+        echo "Adding user $user to Samba."
+        # Add the user to the system if not already added
+        if ! id -u "$user" >/dev/null 2>&1; then
+            adduser --disabled-password --gecos "" "$user"
+        fi
 
-
-# Disable the banner
-echo "server string = " >> $smb_conf
-
-# CHANGE LATER
-
-echo "Type all user account names, with a space in between"
-read -a usersSMB
-usersSMBLength=${#usersSMB[@]}	
-for (( i=0; i < $usersSMBLength; i++ ))
-do
-    echo -e "$password\n$password" | smbpasswd -a "${usersSMB[i]}"
-    printTime "${usersSMB[i]} has been given the password set in the variable for Samba."
-done
+        # Prompt for Samba password
+        echo "Enter Samba password for user $user:"
+        smbpasswd -a "$user"
+        smbpasswd -e "$user"  # Enable the Samba user
+        echo "User $user has been configured for Samba."
+    done < "$users_file"
+else
+    echo "Error: Authorized users file '$users_file' not found."
+    exit 1
+fi
 
 # Restart Samba to apply changes
 systemctl reload smbd
-echo "Samba has been configured."
+
+echo "Samba has been successfully configured."
